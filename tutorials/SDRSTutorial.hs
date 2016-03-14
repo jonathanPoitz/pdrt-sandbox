e17f1f0bc5866fd6cb426126c104eee815c7e4e0
Jonathan Poitz <jonathanpoitz@gmail.com>
2016-03-14 23:10:05 +0100

    more examples

--- tutorials/SDRSTutorial.hs
+++ tutorials/SDRSTutorial.hs
@@ -5,5 +5,23 @@ import Data.SDRS
 
 map1 = Map.fromList [(1,"a"),(2,"b"),(3,"c"),(4,"d")]
 
+-- | "A man is happy."
+drs1 = DRS [DRSRef "x"] 
+                  [Rel (DRSRel "man") [DRSRef "x"]
+                  ,Rel (DRSRel "happy") [DRSRef "x"]]
 
-sdrs1 = SDRS (Map.fromList [(0, DRSForm $ DRS [] [])]) 0
+-- | "A woman is sad."
+drs2 = DRS [DRSRef "x"] 
+                  [Rel (DRSRel "woman") [DRSRef "x"]
+                  ,Rel (DRSRel "sad") [DRSRef "x"]]
+
+-- mergeDRS3 = Merge drs1 drs2
+-- resolvedDRS3 = Data.DRS.drsResolveMerges $ mergeDRS3
+
+
+
+sdrs1 = SDRS (Map.fromList [(0, DRSForm $ drs1),
+														(1, DRSForm $ drs2),
+														(2, RRel Parallel 0 1)]) 0
+
+sdrs3 = SDRS (Map.fromList [(0, DRSForm $ DRS [] [])]) 0
