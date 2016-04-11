c8d54745ec99254030aee108f164b17a1e5abd49
JonathanPoitz <jonathanpoitz@gmail.com>
2016-04-11 19:33:10 +0200

    beginning implementation of function checking whether two SDRSs are isomorph, i.e. that they're identical on the level of discourse structure and DRS contents but not necessarily on the individual labels used

--- src/Data/SDRS/Properties.hs
+++ src/Data/SDRS/Properties.hs
@@ -17,6 +17,7 @@ module Data.SDRS.Properties
 , sdrsAllDRSRefUnique
 , allRelationsValid
 , validLast
+, isomorph
 ) where
 
 import Data.SDRS.DataType
@@ -92,7 +93,72 @@ validLast s@(SDRS m l) = isSegment (m M.! l) &&
         isSegment _           = False -- isn't there an easier way? but idk how to pattern match on Segment when not in a function. note, this also ignores the possibility that the last node is introduced in a rec. SDRSFormula
         allRelations = map snd $ relations s
 
+---------------------------------------------------------------------------
+-- | Checks whether two 'SDRS's @s1@ and @s2@ are isomorph, i.e., their
+-- graph structure does not differ except for different labeling of DUs
+--
+-- TODO: account for associativity:
+-- And (And (Relation _ 1 2) (Relation _ 3 4)) (Relation _ 5 6)) should be isomorphic to
+-- And (Relation _ 1 2) (And (Relation _ 3 4) (Relation _ 5 6))
+---------------------------------------------------------------------------
+--isomorph :: SDRS -> SDRS -> Bool
+--isomorph s1@(SDRS m1 _) s2@(SDRS m2 _) = isomorph' (root s1) (root s2)
+--  where g1 = discourseGraph s1
+--        g2 = discourseGraph s2
+--        isomorph' :: [DisVar] -> [DisVar] -> Bool
+--        isomorph' (cur1:rest1) (cur2:rest2)
+--          | ((M.lookup cur1 g1) == Nothing) ||
+--            ((M.lookup cur2 g2) == Nothing) = ((m1 M.! cur1) `isomorphSF` (m2 M.! cur2)) &&
+--                                              (isomorph' rest1 rest2)
+--          | otherwise                       = (m1 M.! cur1) `isomorphSF` (m2 M.! cur2) &&
+--                                              isomorph' (map fst $ g1 M.! cur1) (map fst $ g2 M.! cur2) &&
+--                                              isomorph' rest1 rest2
+--        isomorph' [] []                     = True
+--        isomorph' _ []                      = False
+--        isomorph' [] _                      = False
+--        isomorphSF :: SDRSFormula -> SDRSFormula -> Bool
+--        isomorphSF (Segment d1) (Segment d2) = 
+--          d1 == d2 -- extend to account for different DisRefs in DRSs
+--        isomorphSF (Relation l1 dv1 dv2) (Relation l2 dv3 dv4) = 
+--          (labelEq l1 l2) &&
+--          (m1 M.! dv1) `isomorphSF` (m2 M.! dv3) &&
+--          (m1 M.! dv2) `isomorphSF` (m2 M.! dv4)
+--        isomorphSF (And sf1 sf2) (And sf3 sf4) = 
+--          (sf1 `isomorphSF` sf3 && sf2 `isomorphSF` sf4) ||
+--          (sf1 `isomorphSF` sf4 && sf2 `isomorphSF` sf3) -- TODO account for associativity (see function comment on top)
+--        isomorphSF (Not sf1) (Not sf2) =
+--          sf1 `isomorphSF` sf2
+--        isomorphSF _ _ = False
 
+---------------------------------------------------------------------------
+-- current problem is that the order of outscoped SDRSFormula (2 5 2 7 vs. 20 8 20 5) clashes.
+---------------------------------------------------------------------------
 
-
+isomorph :: SDRS -> SDRS -> [String]
+isomorph s1@(SDRS m1 _) s2@(SDRS m2 _) = isomorph' (root s1) (root s2)
+  where g1 = discourseGraph s1
+        g2 = discourseGraph s2
+        isomorph' :: [DisVar] -> [DisVar] -> [String]
+        isomorph' (cur1:rest1) (cur2:rest2)
+          | ((M.lookup cur1 g1) == Nothing) || -- || or &&? 
+            ((M.lookup cur2 g2) == Nothing) = (show cur1 ++ show cur2 ++ show ((m1 M.! cur1) `isomorphSF` (m2 M.! cur2))) : (isomorph' rest1 rest2)
+          | otherwise = (show cur1 ++ show cur2 ++ show ((m1 M.! cur1) `isomorphSF` (m2 M.! cur2))) :
+                        (isomorph' (map fst $ g1 M.! cur1) (map fst $ g2 M.! cur2)) ++
+                        (isomorph' rest1 rest2)
+        isomorph' [] []                     = [show True]
+        isomorph' _ []                      = [show False]
+        isomorph' [] _                      = [show False]
+        isomorphSF :: SDRSFormula -> SDRSFormula -> Bool
+        isomorphSF (Segment d1) (Segment d2) = 
+          d1 == d2 -- extend to account for different DisRefs in DRSs
+        isomorphSF (Relation l1 dv1 dv2) (Relation l2 dv3 dv4) = 
+          (labelEq l1 l2) &&
+          (m1 M.! dv1) `isomorphSF` (m2 M.! dv3) &&
+          (m1 M.! dv2) `isomorphSF` (m2 M.! dv4)
+        isomorphSF (And sf1 sf2) (And sf3 sf4) = 
+          (sf1 `isomorphSF` sf3 && sf2 `isomorphSF` sf4) ||
+          (sf1 `isomorphSF` sf4 && sf2 `isomorphSF` sf3) -- TODO account for associativity (see function comment on top)
+        isomorphSF (Not sf1) (Not sf2) =
+          sf1 `isomorphSF` sf2
+        isomorphSF _ _ = False
 
