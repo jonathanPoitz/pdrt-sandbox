9065f341a78f7649fabd89463ac71dcd5d071b47
JonathanPoitz <jonathanpoitz@gmail.com>
2016-05-04 00:19:15 +0200

    fixed addDRS, first version to support relation update with swapping around of relations

--- src/Data/SDRS/Composition.hs
+++ src/Data/SDRS/Composition.hs
@@ -14,13 +14,15 @@ module Data.SDRS.Composition
 (
   buildFromDRSs
 , addDRS
+, removeRel
 ) where
 
 import Data.SDRS.DataType
 import qualified Data.Map as M
 import Data.SDRS.DiscourseGraph
 import Data.SDRS.Structure (lookupKey)
--- import Debug.Trace
+--import Debug.Trace
+--import Data.SDRS.Show()
 
 ---------------------------------------------------------------------------
 -- | Build new SDRS using two DRSs and their relation
@@ -78,24 +80,23 @@ buildFromDRSs rel d1 d2 = SDRS (M.fromList [(0, Relation rel 1 2),
 ---------------------------------------------------------------------------
 -- | adds a 'DRS' to an 'SDRS' given a number of edges, represented by
 -- tuples consisting of the target node and the relation to that node.
--- TODO, needs to be debugged, e.g. addDRS sdrsfullal07_to4 _al07_5 [(2,relationFromLabel "Narration")] yields wrong result. (can't handle removal of relations from an and)
--- TODO disgustingly complicated, simplify!
 ---------------------------------------------------------------------------
 addDRS :: SDRS -> DRS -> [(DisVar,SDRSRelation)] -> SDRS
 addDRS s@(SDRS m _) d edges = SDRS updatedMap updatedLast
   where updatedLast = (fst $ M.findMax m) + 1 -- new reference to last 
-        updatedMap = M.insert updatedLast (Segment d) (updateRelations edges m updatedLast) -- the new map with the added segment and the updated relations
+        updatedMap = updateRelations edges mWithSegment updatedLast -- the new map with the added segment and the updated relations
+        mWithSegment = M.insert updatedLast (Segment d) m -- 1. step - new segment
         updateRelations :: [(DisVar, SDRSRelation)] -> M.Map DisVar SDRSFormula -> DisVar -> M.Map DisVar SDRSFormula
-        updateRelations [] m' _                     = m'
-        updateRelations ((dv, rel):rest) m' maxKey
-          | isOnRF dv && isRoot dv                  = updateRelations rest (addNewRel m') (maxKey + 1) -- add it to the root node, put this relation under new outscoping label
+        updateRelations [] mArg _                     = mArg
+        updateRelations ((dv, rel):rest) mArg maxKey
+          | isOnRF dv && isRoot dv                  = updateRelations rest mWithSwapRels updatedOutscope
           | isOnRF dv && isCrd rel &&
-            isTopic rel && entailsTopic rel         = updateRelations rest (addNewRel updatedRefs) (maxKey + 1) -- put new rel under new outscope and update old references to left arg with new outscoping label
+            isTopic rel && entailsTopic rel         = updateRelations rest mWithSwapRels updatedOutscope
           | isOnRF dv && isCrd rel && 
-            isTopic rel && (not $ entailsTopic rel) = updateRelations rest (addNewRel updatedRefs) (maxKey + 1) -- not sure, we might have to make an explicit \Downarrow relation
+            isTopic rel && (not $ entailsTopic rel) = updateRelations rest mWithSwapRels updatedOutscope -- not sure, we might have to make an explicit \Downarrow relation
           | isOnRF dv && isCrd rel                  = updateRelations rest mapWithNewConj (maxKey + 1)
           | isOnRF dv                               = updateRelations rest mapWithNewConj (maxKey + 1)
-          | otherwise                               = updateRelations rest m' maxKey -- skipping relation b/c target node is not on RF of SDRS
+          | otherwise                               = updateRelations rest mArg maxKey -- skipping relation b/c target node is not on RF of SDRS
           where isOnRF :: DisVar -> Bool
                 isOnRF dv' = dv' `elem` rf s
                 isRoot :: DisVar -> Bool
@@ -104,32 +105,72 @@ addDRS s@(SDRS m _) d edges = SDRS updatedMap updatedLast
                 isCrd rel' = relType rel' == Crd
                 entailsTopic :: SDRSRelation -> Bool
                 entailsTopic _ = True -- TODO implement, which subRels entail \Downarrow?
-                updatedRefs = updateRefs dv (maxKey + 1) m'
-                addNewRel :: M.Map DisVar SDRSFormula -> M.Map DisVar SDRSFormula
-                addNewRel m'' = M.insert (maxKey + 1) (Relation rel dv updatedLast) m''
-                mapWithNewConj = M.adjust (And (Relation rel dv updatedLast)) (lookupKey s dv) m' -- add existing SDRSFormula with And to the new one. TODO, order? The old thing should be the first arg of And, right?
-                updateRefs :: DisVar -> DisVar -> M.Map DisVar SDRSFormula -> M.Map DisVar SDRSFormula
-                updateRefs old new m'' = M.map updateRef m''
-                  where updateRef :: SDRSFormula -> SDRSFormula
-                        updateRef seg@(Segment {})      = seg
-                        updateRef r@(Relation rel' dv1 dv2)
-                          | dv1 == old && dv2 == old    = Relation rel' new new -- shouldn't happen
-                          | dv1 == old                  = Relation rel' new dv2
-                          | dv2 == old                  = Relation rel' dv1 new -- this should then be replaced by the new outscoping label and put underneath that. how the hell will i do that?
-                          | otherwise                   = r
-                        updateRef (And sf1 sf2)         = And (updateRef sf1) (updateRef sf2)
-                        updateRef (Not sf1)             = Not (updateRef sf1)
-
+                -----------
+                updatedOutscope = maxKey + 1
+                mWithRel = M.insert updatedOutscope (Relation rel dv updatedLast) mArg -- 2. step - new relation
+                mWithRightArgUpdate = updateRightArgs mWithRel dv updatedOutscope -- 3. step - update all occurrences of dv as a right arg of a relation by replacing it with new outscoping label
+                swapRels = calcSwapRels mArg dv
+                mWithRemovedSwapRels = removeOldSwapRels updatedOutscope swapRels mWithRightArgUpdate
+                mWithSwapRels = addSwapRelsToMap updatedOutscope swapRels mWithRemovedSwapRels
+                mapWithNewConj = M.adjust (flip And (Relation rel dv updatedLast)) (lookupKey s dv) mArg -- add existing SDRSFormula with And to the new one.
+                removeOldSwapRels :: DisVar -> [SDRSFormula] -> M.Map DisVar SDRSFormula -> M.Map DisVar SDRSFormula
+                removeOldSwapRels _ [] mArg'                        = mArg'
+                removeOldSwapRels new (r@(Relation {}):rest') mArg' = removeOldSwapRels new rest' (removeSwapRel r mArg')
+                  where removeSwapRel :: SDRSFormula -> M.Map DisVar SDRSFormula -> M.Map DisVar SDRSFormula
+                        removeSwapRel r'@(Relation {}) mArg'' = M.map (removeRel r') mArg'' -- remove everywhere but in new one
+                        removeSwapRel _ mArg''                = mArg''
+                removeOldSwapRels new (_:rest') mArg'               = removeOldSwapRels new rest' mArg'
+                -----------
+                addSwapRelsToMap :: DisVar -> [SDRSFormula] -> M.Map DisVar SDRSFormula -> M.Map DisVar SDRSFormula
+                addSwapRelsToMap _ [] mArg'                        = mArg'
+                addSwapRelsToMap new (r@(Relation {}):rest') mArg' = addSwapRelsToMap new rest' (M.adjust (And r) new mArg') -- TODO switch args around?
+                addSwapRelsToMap new (_:rest') mArg'               = addSwapRelsToMap new rest' mArg'
+                -----------
+                calcSwapRels :: M.Map DisVar SDRSFormula -> DisVar -> [SDRSFormula]
+                calcSwapRels mArg' old = M.foldl putSwapRel [] mArg'
+                  where putSwapRel :: [SDRSFormula] -> SDRSFormula -> [SDRSFormula]
+                        putSwapRel acc (Segment {})  = acc
+                        putSwapRel acc r@(Relation _ dv1 _)
+                          | dv1 == old               = r:acc
+                          | otherwise                = acc
+                        putSwapRel acc (And sf1 sf2) = putSwapRel (putSwapRel acc sf1) sf2
+                        putSwapRel acc (Not sf1)     = putSwapRel acc sf1
+                -----------
+                updateRightArgs :: M.Map DisVar SDRSFormula -> DisVar -> DisVar -> M.Map DisVar SDRSFormula
+                updateRightArgs mArg' old new = M.map updateR mArg'
+                  where updateR :: SDRSFormula -> SDRSFormula
+                        updateR seg@(Segment {}) = seg
+                        updateR r@(Relation rel' dv1 dv2)
+                          | dv2 == old           = Relation rel' dv1 new
+                          | otherwise            = r
+                        updateR (And sf1 sf2)    = And (updateR sf1) (updateR sf2)
+                        updateR (Not sf1)        = Not (updateR sf1)
                       
 
+---------------------------------------------------------------------------
+-- | Private
+---------------------------------------------------------------------------
 
-
-
-
-
-
-
-
+---------------------------------------------------------------------------
+-- | Given a conjunction of 'SDRSFormula'e @sf@, removes the subrelation @r@
+-- from the conjunction.
+---------------------------------------------------------------------------
+removeRel :: SDRSFormula -> SDRSFormula -> SDRSFormula
+removeRel r@(Relation {}) sf@(And {}) = recurse sf
+  where recurse :: SDRSFormula -> SDRSFormula
+        recurse (And sf1@(And {}) sf2@(And {})) = And (recurse sf1) (recurse sf2)
+        recurse (And sf1@(And {}) sf2@(Relation {}))
+          | r == sf2                            = recurse sf1
+          | otherwise                           = And (recurse sf1) sf2
+        recurse (And sf1@(Relation {}) sf2@(And {}))
+          | r == sf1                            = recurse sf2
+          | otherwise                           = And sf1 (recurse sf2)
+        recurse a@(And sf1@(Relation {}) sf2@(Relation {}))
+          | r == sf1                            = sf2
+          | r == sf2                            = sf1
+          | otherwise                           = a
+        recurse sf'                             = sf'
+removeRel _ sf = sf
 
 
 
