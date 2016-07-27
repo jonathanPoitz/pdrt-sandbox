83e0dda62adaf315056f2a82721779f6219f3d3d
Jonathan Poitz <jonathanpoitz@gmail.com>
2016-07-27 14:56:14 +0200

    fix sdrsProperDRSs, removed sfsSemIsomorphic

--- src/Data/SDRS/Properties.hs
+++ src/Data/SDRS/Properties.hs
@@ -17,7 +17,7 @@ module Data.SDRS.Properties
 , sdrsUniqueDRSRefs
 , validLast
 , sfsStrucIsomorphic
-, sfsSemIsomorphic
+--, sfsSemIsomorphic
 , isWellformedSDRS
 ) where
 
@@ -49,8 +49,8 @@ sdrsProperDRSs s = proper $ segments s
        proper ((dv,EDU d):rest)     = (properDRS dv d) && proper rest
        proper ((_,_):rest)          = proper rest
        properDRS :: DisVar -> DRS -> Bool
-       properDRS dv d = isProperDRS ((foldl (<<+>>) (DRS [] []) accDRSs) <<+>> d) -- is merging with empty DRS the only way for this?
-         where accDRSs = accessibleDRSs s dv
+       properDRS dv d = isProperDRS ((foldl (<<+>>) (DRS [] []) revAccDRSs) <<+>> d)
+         where revAccDRSs = reverse $ accessibleDRSs s dv -- needs to be reversed to get correct order of merges
        
 ---------------------------------------------------------------------------
 -- | Checks, given 'SDRS' s@@, whether each of its embedded 'DRS's @d@ is /pure/, where:
@@ -94,7 +94,7 @@ validLast s@(SDRS m l)
                               not (any (\(CDU (Relation _ dv _)) -> dv == l) allRelations)
   where isSegment :: SDRSFormula -> Bool
         isSegment (EDU _) = True
-        isSegment _       = False -- isn't there an easier way? but idk how to pattern match on EDU when not in a function. note, this also ignores the possibility that the last node is introduced in a rec. SDRSFormula
+        isSegment _       = False -- use case instead? but watch out for npe
         allRelations = map snd $ relations s
 
 ---------------------------------------------------------------------------
@@ -111,36 +111,7 @@ sfsStrucIsomorphic (SDRS m _) dv1 dv2
   where sf1 = m M.! dv1
         sf2 = m M.! dv2
         comp :: SDRSFormula -> SDRSFormula -> Bool
-        comp (EDU _) (EDU _) = True
-        comp (CDU cdu1) (CDU cdu2) = compareCDU cdu1 cdu2
-        comp _ _ = False
-        compareCDU :: CDU -> CDU -> Bool
-        compareCDU (Relation rel1 _ _) (Relation rel2 _ _)
-          | rel1 == rel2 = True
-          | otherwise    = False
-        compareCDU (And a1 a1') (And a2 a2') = compareCDU a1 a2 &&
-                                               compareCDU a1' a2'
-        compareCDU (Not n1) (Not n2) = compareCDU n1 n2
-        compareCDU _ _ = False
-
----------------------------------------------------------------------------
--- | Checks whether two subgraphes within the same 'SDRS's @s@ are structurally
--- isomorphic, i.e., their graph structure does not differ except for different
--- labeling of DUs. These subgraphes are determined by their root nodes,
--- two 'DisVar's @dv1@ and @dv2@ that are passed along to this function as well. 
----------------------------------------------------------------------------
-sfsSemIsomorphic :: SDRS -> DisVar -> DisVar -> Bool
-sfsSemIsomorphic (SDRS m _) dv1 dv2
-  | M.lookup dv1 m == Nothing ||
-    M.lookup dv2 m == Nothing = False
-  | otherwise                 = comp sf1 sf2
-  where sf1 = m M.! dv1
-        sf2 = m M.! dv2
-        comp :: SDRSFormula -> SDRSFormula -> Bool
-        comp (EDU e1) (EDU e2)
-          | e1 == e2  = True
-          -- ^ This is the strict version, here it would be convenient to have a function drsAlphaEquivalent
-          | otherwise = False
+        comp (EDU _) (EDU _) = True -- check here for semantic isomorphism
         comp (CDU cdu1) (CDU cdu2) = compareCDU cdu1 cdu2
         comp _ _ = False
         compareCDU :: CDU -> CDU -> Bool
@@ -155,22 +126,13 @@ sfsSemIsomorphic (SDRS m _) dv1 dv2
 ---------------------------------------------------------------------------
 -- | Checks for the well-formedness of an 'SDRS'.
 ---------------------------------------------------------------------------
---isWellformedSDRS :: SDRS -> Bool
---isWellformedSDRS s = validLast s &&
---                     noSelfRefs s &&
---                     sdrsPureDRSs s &&
---                     sdrsProperDRSs s &&
---                     allRelArgsBound s &&
---                     allSegmentsBound s
-
-
-isWellformedSDRS :: SDRS -> [Bool]
-isWellformedSDRS s = validLast s :
-                     noSelfRefs s :
-                     sdrsPureDRSs s :
-                     sdrsProperDRSs s :
-                     allRelArgsBound s :
-                     allSegmentsBound s : []
+isWellformedSDRS :: SDRS -> Bool
+isWellformedSDRS s = validLast s &&
+                     noSelfRefs s &&
+                     sdrsPureDRSs s &&
+                     sdrsProperDRSs s &&
+                     allRelArgsBound s &&
+                     allSegmentsBound s
 
 ---------------------------------------------------------------------------
 -- | Checks whether all Segments are attached to a node that's on the RF of
